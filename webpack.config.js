b0ccd016719c34be36150ee259f3dbb92242eb32
Ian Bolton <ibolton@redhat.com>
2022-09-30 15:44:49 -0400

    wip mfe

--- webpack.config.js
+++ webpack.config.js
@@ -38,23 +38,23 @@ module.exports = {
       // version is inferred from package.json
       // there is no version check for the required version
       // so it will always use the higher version found
-      shared: {
-        react: {
-          requiredVersion: deps.react,
-          import: "react", // the "react" package will be used a provided and fallback module
-          shareKey: "react", // under this name the shared module will be placed in the share scope
-          shareScope: "default", // share scope with this name will be used
-          singleton: true, // only a single version of the shared module is allowed
-        },
-        "react-dom": {
-          requiredVersion: deps["react-dom"],
-          singleton: true, // only a single version of the shared module is allowed
-        },
-        // adds moment as shared module
-        // version is inferred from package.json
-        // it will use the highest moment version that is >= 2.24 and < 3
-        moment: deps.moment,
-      },
+      // shared: {
+      //   react: {
+      //     requiredVersion: deps.react,
+      //     import: "react", // the "react" package will be used a provided and fallback module
+      //     shareKey: "react", // under this name the shared module will be placed in the share scope
+      //     shareScope: "default", // share scope with this name will be used
+      //     singleton: true, // only a single version of the shared module is allowed
+      //   },
+      //   "react-dom": {
+      //     requiredVersion: deps["react-dom"],
+      //     singleton: true, // only a single version of the shared module is allowed
+      //   },
+      //   // adds moment as shared module
+      //   // version is inferred from package.json
+      //   // it will use the highest moment version that is >= 2.24 and < 3
+      //   moment: deps.moment,
+      // },
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
